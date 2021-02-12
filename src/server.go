10c97f652f87bbd22dcec23e89e74ccdbcfddc21
CREYPE <costa.reype@ynov.com>
2021-02-12 17:32:25 +0100

    Squelette du serveur.go

--- src/server.go
+++ src/server.go
@@ -6,15 +6,20 @@ func main() {
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
 	http.HandleFunc("/", serveHome)
 
-	fmt.Println("Start... ")
+	http.HandleFunc("/Posts", AllPosts)
+	http.HandleFunc("/Post", OnePost)
+	http.HandleFunc("/Profil", Profil)
+	http.HandleFunc("/Login", Login)
+	http.HandleFunc("/Register", Register)
 
+	fmt.Println("Start... ")
 	http.ListenAndServe(":8080", nil)
 
 }
 
 // servHome : //* Page d'acceuil
 func serveHome(w http.ResponseWriter, r *http.Request) {
-	files := []string{"template/Home.html", "template/Nav.html"}
+	files := []string{"template/Home.html", "template/Common.html"}
 
 	tmp, err := template.ParseFiles(files...)
 	if err != nil {
@@ -28,4 +33,24 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Server Error", 500)
 	}
 
+}
+
+func AllPosts(w http.ResponseWriter, r *http.Request){
+	files := []string{"template/AllPosts.html", "template/Common.html"}
+}
+
+func OnePost(w http.ResponseWriter, r *http.Request){
+	files := []string{"template/OnePost.html", "template/Common.html"}
+}
+
+func Profil(w http.ResponseWriter, r *http.Request){
+	files := []string{"template/Profil.html", "template/Common.html"}
+}
+
+func Login(w http.ResponseWriter, r *http.Request){
+	files := []string{"template/Login.html", "template/Common.html"}
+}
+
+func Register(w http.ResponseWriter, r *http.Request){
+	files := []string{"template/Register.html", "template/Common.html"}
 }
